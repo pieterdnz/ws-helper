2b34e3b23a607311f37683b4e6cbe04e88c2140d
pieter bos <pieter@denieuwezaak.nl>
2020-01-27 09:42:06 +0100

    test postinstall

---
+++ test.js
@@ -0,0 +1,12 @@
+var fs = require("fs");
+
+var path = "./";
+
+fs.readdir(path, function(err, items) {
+  console.log(items);
+
+  for (var i = 0; i < items.length; i++) {
+    console.log(items[i]);
+  }
+  console.log("OEPS");
+});
